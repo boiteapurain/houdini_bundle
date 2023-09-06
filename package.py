69e1f89cfc36607fe0371ee9adefdd8e1aac30d0
boitapurain <mathieu.negrel82@gmail.com>
2023-09-06 11:28:05 +0200

    super

--- package.py
+++ package.py
@@ -1,37 +1,39 @@
 # -*- coding: utf-8 -*-
 
-name = 'houdini_bundle'
+name = "houdini_bundle"
 
-version = '19.5.403.std.1.1'
+version = "19.5.403.std.1.1"
 
-description = 'Houdini Side Fx package.'
+description = "Houdini Side Fx package."
 
-authors = ['It']
+authors = ["It"]
 
-variants = [['~python-3.9']]
+variants = [["~python-3.9"]]
+
+build_command = 'python {root}/build.py {install}'
+
+release_command = 'python {root}/release.py {install}'
+
 
 def commands():
     global system
     global env
-    
+
     software = "19.5.403"
-    
+
     if system.platform == "linux":
         # NO GPU
         env.HOUDINI_OCL_DEVICENUMBER.append(0)
         env.HOUDINI_OCL_DEVICETYPE.append("CPU")
         env.HOUDINI_USE_HFS_OCL.append(0)
-    
+
         # HOUDINI BASIC CONFIG
         env.HFS.append(f"/opt/hfs{software}")
-    
+
         # PATH
         env.PATH.append(f"/opt/hfs{software}/bin")
         env.PATH.append(f"/opt/hfs{software}/python/bin")
-    
+
         # CMAKE CONFIG
         env.CMAKE_PREFIX_PATH.append(f"/opt/hfs{software}/toolkit/cmake")
 
-timestamp = 1693670763
-
-format_version = 2
--- release.py
+++ release.py
@@ -5,12 +5,12 @@ import os.path
 import sys
 
 
-def build(source_path, build_path, install_path, targets):
+def release(source_path, build_path, install_path, targets):
     pass
 
 
 if __name__ == '__main__':
-    build(
+    release(
         source_path=os.environ['REZ_BUILD_SOURCE_PATH'],
         build_path=os.environ['REZ_BUILD_PATH'],
         install_path=os.environ['REZ_BUILD_INSTALL_PATH'],
